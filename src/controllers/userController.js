1be18c9d28ecbf84e792720eda442f4c23309662
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-23 14:04:32 -0300

    feat: Add CRUD routes and controller methods for User model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,6 +29,50 @@ const userController = {
       res.status(400).send(error)
     }
   },
+  async getUser(req, res) {
+    try {
+      const response = await UserModel.findById(req.params.id)
+      if (!response) {
+        res.status(404).send({ message: 'User not found' })
+        return
+      }
+      res.status(200).send(response)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  },
+  async delete(req, res) {
+    try {
+      const response = await UserModel.findByIdAndDelete(req.params.id)
+      if (!response) {
+        res.status(404).send({ message: 'User not found' })
+        return
+      }
+      res.status(200).send({ message: 'User deleted' })
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  },
+  async update(req, res) {
+    try {
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+      }
+      const response = await UserModel.findByIdAndUpdate(req.params.id, user, {
+        new: true,
+      })
+      if (!response) {
+        res.status(404).send({ message: 'User not found' })
+        return
+      }
+      res.status(200).send(response)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  },
 }
 
 module.exports = userController
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,5 +3,8 @@ const userController = require('../controllers/userController')
 
 router.route('/register').post((req, res) => userController.create(req, res))
 router.route('/users').get((req, res) => userController.getAll(req, res))
+router.route('/users/:id').get((req, res) => userController.getUser(req, res))
+router.route('/users/:id').delete((req, res) => userController.delete(req, res))
+router.route('/users/:id').put((req, res) => userController.update(req, res))
 
 module.exports = router
