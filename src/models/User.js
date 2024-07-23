e0eebd0c8952a75073c73359795482c52d36ae5a
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-23 13:19:49 -0300

    feat: Add User model for user information

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  role: {
+    type: String,
+    required: [true, 'Role is required'],
+    enum: ['admin', 'user'],
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User, userSchema }
