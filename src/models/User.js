0464ee9a0440a8254c351dd0009c549de8ab301b
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-23 13:38:56 -0300

    feat: Add validation for name and email fields in User model

--- src/models/User.js
+++ src/models/User.js
@@ -7,10 +7,18 @@ const userSchema = new Schema(
     name: {
       type: String,
       required: [true, 'Name is required'],
+      minLength: [3, 'Name must be at least 2 characters'],
     },
     email: {
       type: String,
       required: [true, 'Email is required'],
+      unique: true,
+      validate: {
+        validator: (value) => {
+          return /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(value)
+        },
+        message: (props) => `${props.value} is not a valid email address`,
+      },
     },
     password: {
       type: String,
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
 
-router.route('/users').post((req, res) => userController.create(req, res))
+router.route('/register').post((req, res) => userController.create(req, res))
 
 module.exports = router
