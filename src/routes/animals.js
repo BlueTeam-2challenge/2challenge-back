3c65d1710dbfdef23627ff434015ec1fde94410e
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-23 12:11:56 -0300

    chore: Add GET route to retrieve all animals

--- src/routes/animals.js
+++ src/routes/animals.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const animalController = require('../controllers/animalController')
 
 router.route('/animals').post((req, res) => animalController.create(req, res))
+router.route('/animals').get((req, res) => animalController.getAll(req, res))
 
 module.exports = router
